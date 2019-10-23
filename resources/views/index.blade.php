8a5ae99f4db87b99b58d1b1f4c908a54f21d2272
thang <nguyenthang011099.hust@gmail.com>
2019-10-23 14:24:35 +0700

    create chart read data from mysql

---
+++ resources/views/index.blade.php
@@ -0,0 +1,86 @@
+<!doctype html>
+<html lang="{{ config('app.locale') }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
+
+    <!-- Fonts -->
+    <link href="{{asset('css/app.css')}}" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.12.3/css/bootstrap-select.min.css">
+</head>
+<body>
+<div class="row">
+    <div class="col-md-10 col-md-offset-1">
+        <div class="panel panel-default">
+          <div class="panel-heading">
+              <h2 style="text-align:left">Weather Forecast</h2>
+          </div>
+            <div class="panel-body">
+                <canvas id="canvas" height="270" width="600"></canvas>
+            </div>
+        </div>
+    </div>
+</div>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.12.3/js/bootstrap-select.min.js" charset="utf-8"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.6.0/Chart.bundle.js" charset="utf-8"></script>
+<script>
+    var url = "{{url('stock/chart')}}";
+    var Years = new Array();
+    var Labels = new Array();
+    var Wind = new Array();
+    var Temperature= new Array();
+    var Humid= new Array();
+    $(document).ready(function(){
+        $.get(url, function(response){
+            response.forEach(function(data){
+                Years.push(data.id);
+                Labels.push(data.stockName);
+                Wind.push(data.Wind);
+                Temperature.push(data.Temperature);
+                Humid.push(data.Humid);
+            });
+            var ctx = document.getElementById("canvas").getContext('2d');
+            var myChart = new Chart(ctx, {
+                type: 'line',
+                data: {
+                    labels:Years,
+                    datasets: [{
+                        label: 'Wind',
+                        data: Wind,
+                        borderColor:'rgba(42,176,17)',
+                        borderWidth: 3
+                    },
+                        {
+                            label: 'Temperature',
+                            data: Temperature,
+                            borderColor:'rgba(236,74,16)',
+                            borderWidth: 3
+                        },
+                        {
+                            label: 'Humid',
+                            data: Humid,
+                            borderColor:'rgba(16,140,240)',
+                            borderWidth: 3
+                        }
+                    ]
+                },
+                options: {
+                    scales: {
+                        yAxes: [{
+                            ticks: {
+                                beginAtZero:true
+                            }
+                        }]
+                    }
+                }
+            });
+        });
+    });
+</script>
+</body>
+</html>
